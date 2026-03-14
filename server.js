6a6cc19d334ef3944b48c7543d165ee9710b2585
IngSamuDev <samuelgarcia110106@gmail.com>
2026-03-14 16:33:38 -0500

    Proyecto Completo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const start = async () => {
     await initDB();
     app.listen(PORT, () => {
-        console.log(`Servidor corriendo en puerto ${PORT}`);
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
 };
 
